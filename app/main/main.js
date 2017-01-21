0a2efa0a9f34f53f65b364da86554bb381c0b915
Eryk Rakowski <sentialx@gmail.com>
2017-01-21 14:10:38 +0100

    added support for wexond:// protocol

--- app/main/main.js
+++ app/main/main.js
@@ -1,13 +1,23 @@
-var {app, BrowserWindow, protocol} = require('electron'),
+var {app, BrowserWindow} = require('electron'),
+    protocol = require('electron').protocol,
+    remote = require('electron').remote,
     path = require('path');
-let mainWindow = null;
+
+let mainWindow;
+
 app.on('window-all-closed', () => {
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-app.on('ready', () => {
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+function createWindow() {
     mainWindow = new BrowserWindow({width: 900, height: 700, frame: false, minWidth: 300, minHeight: 430});
     mainWindow.loadURL('file://' + __dirname + '/../app/renderer/public/index.html');
     mainWindow.setMenu(null);
@@ -18,29 +28,32 @@ app.on('ready', () => {
     mainWindow.on('unresponsive', function () {
 
     });
-});
+}
 process.on('uncaughtException', function () {
 
 });
 
-protocol.registerStandardSchemes(['webexpress'])
+protocol.registerStandardSchemes(['wexond']);
 app.on('ready', function () {
-    protocol.registerFileProtocol('webexpress', (request, callback) => {
-        var url = request.url.substr(13)
-        var lastChar = url.substr(url.length - 1)
+    protocol.registerFileProtocol('wexond', (request, callback) => {
+        var url = request.url.substr(8);
+        var lastChar = url.substr(url.length - 1);
         var s = url.split("/");
         if (lastChar != "/") {
-            url = url.replace(s[0], "")
+            url = url.replace(s[0], "");
         }
         if (lastChar == "/") {
-            url = url.substring(0, url.length - 1)
-            url += ".html"
+            url = url.substring(0, url.length - 1);
+            url += ".html";
         }
         callback({
-            path: path.normalize(`${__dirname}/${url}`)
-        })
+            path: path.normalize(`${__dirname}/../app/renderer/public/${url}`)
+        });
     }, (error) => {
-        if (error) console.error('Failed to register protocol')
-    })
+        if (error) console.error('Failed to register protocol');
+    });
     createWindow();
 });
+
+var client = require('electron-connect').client;
+client.create(mainWindow);
