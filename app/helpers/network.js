1f3ff5270e9dd97669681c093a28594ac922c367
Eryk Rakowski <sentialx@gmail.com>
2017-03-28 18:41:04 +0200

    Improve isURL function http://stackoverflow.com/a/5717133

--- app/helpers/network.js
+++ app/helpers/network.js
@@ -30,9 +30,13 @@ export default class Network {
   * @param1 {String} string
   * @return {Boolean}
   */
-  // TODO: this function needs to improve
   static isURL (string) {
-    var regexp = /[a-zA-Z-0-9]+\.[a-zA-Z-0-9]{2,3}/
-    return regexp.test(string)
+    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // ipv4 address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i');
+    return regexp.test(pattern)
   }
 }
